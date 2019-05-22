53da2a407c06403b429baf14ed5ca2097c183d3b
Milan <milan.dhar@gmail.com>
2019-05-22 15:34:19 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
+require 'pry'
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 20}
+  validates :summary, length: {maximum: 50}
+  validates :category, inclusion: {in: %w(Fiction)}
+  validate :clickbait
+
+
+  private
+
+  def clickbait
+
+    if title == nil
+    errors.add(:title, "need a title!")
+    elsif !title.include?("Won't Believe")
+      errors.add(:title, "doesn't include clickbait")
+    end
+  end
+
 end
