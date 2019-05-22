712fa5f69dbbe51bd381e4e9b5e4c9c535253493
github username <github email address>
2019-05-21 17:25:38 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :is_clickbait?
+
+    #learned how to use regex
+     @@clickbait = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+    #none = does-not-include but != .exclude?
+  def is_clickbait?
+    if @@clickbait.none? { |cb| cb.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
+
 end
