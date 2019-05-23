bd464633eac558109f32fd1e6e910f103d998208
Hanna Engel <hengel@alumni.berklee.edu>
2019-05-23 09:22:28 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, length: {maximum: 250}
+  validates :content, length: {minimum: 250}
+ validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+ 
+
+private
+
 end
